3c3f0415b6e417d9c92df511b521fb5f7465bec6
Mateo Olarte <mateo.olarte8@gmail.com>
2017-05-24 15:14:08 -0500

    reto 6 abuela sorda

--- solution.rb
+++ solution.rb
@@ -3,3 +3,13 @@ require "sinatra"
 get "/" do
   erb :index
 end
+
+post "/response" do
+  texto = params[:textoabuela]
+  if texto == texto.upcase
+    @message = { message: "Ahhh si, manzanas!" }
+  else
+    @message = { message: "Habla más duro mijito" }
+  end
+  erb :response
+end
