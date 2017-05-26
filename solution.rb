d7f3ae57b24c2d36e18ed279cc7f61e00c7fe659
Mateo Olarte <mateo.olarte8@gmail.com>
2017-05-26 12:07:49 -0500

    reto cual es mi navegador?

--- solution.rb
+++ solution.rb
@@ -1,13 +1,5 @@
 require "sinatra"
 
-counter = 0
 get "/" do
-  @count = counter
-  erb :index
-end
-
-post "/" do
-  counter += params[:number].to_i
-  @count = counter
-  erb :index
+  "#{request.user_agent}"
 end
